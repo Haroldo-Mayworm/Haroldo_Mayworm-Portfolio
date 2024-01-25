fe97185d8fcf51a2ab14bbb008b1e442cfffea85
Haroldo H. M. Mayworm <haroldomayworm@gmail.com>
2024-01-25 17:28:24 -0300

    Chore: Rebuild styles
    Review and rebuild the style files. This commit captures the changes made to the project's style, ensuring a fresh and improved appearance.

---
+++ assets/JS/script.js
@@ -0,0 +1,56 @@
+/*
+ * Function that monitors page scroll position and adds/removes classes to create a sticky header effect.
+ */
+window.addEventListener('scroll', function () {
+  var header = document.getElementById('header');
+
+  if (window.scrollY > 0) {
+    header.classList.add('activeScroll-header');
+  } else {
+    header.classList.remove('activeScroll-header');
+  }
+});
+// Final
+
+/*
+ * Function that monitors when a user clicks on one of the menu items. When clicked, it adds/removes the "active" class, changing the item's style.
+ */
+var itemMenu = document.querySelectorAll('.menu ul li a');
+itemMenu.forEach(function (item) {
+  item.addEventListener('click', function () {
+    itemMenu.forEach(function (item) {
+      item.classList.remove('active');
+    });
+    item.classList.add('active');
+  });
+});
+// Final
+
+/* 
+* The function aims to smooth out the scroll when the user clicks on the menu item and return to the top of the page when the "home" item has been clicked.
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  var menuLinks = document.querySelectorAll('.menu a');
+
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+      var targetId = this.getAttribute('href').substring(1);
+      var targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        if (targetId === 'header') {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          });
+        } else {
+          window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth',
+          });
+        }
+      }
+    });
+  });
+});

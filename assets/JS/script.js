ab5e4854db1dbbb8c272b92ce5533a50b8481ce0
Haroldo H. M. Mayworm <haroldomayworm@gmail.com>
2024-01-26 18:14:34 -0300

    Feat: Implement responsive styles for the website
    Integrate responsive styles to adapt your site's appearance across devices. This commit adds the necessary styles to ensure responsiveness across multiple screens.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,12 +2,14 @@
  * Function that monitors page scroll position and adds/removes classes to create a sticky header effect.
  */
 window.addEventListener('scroll', function () {
-  var header = document.getElementById('header');
+  let header = document.getElementById('header');
 
-  if (window.scrollY > 0) {
-    header.classList.add('activeScroll-header');
-  } else {
-    header.classList.remove('activeScroll-header');
+  if (window.innerWidth > 750) {
+    if (window.scrollY > 0) {
+      header.classList.add('activeScroll-header');
+    } else {
+      header.classList.remove('activeScroll-header');
+    }
   }
 });
 // Final
@@ -15,28 +17,30 @@ window.addEventListener('scroll', function () {
 /*
  * Function that monitors when a user clicks on one of the menu items. When clicked, it adds/removes the "active" class, changing the item's style.
  */
-var itemMenu = document.querySelectorAll('.menu ul li a');
-itemMenu.forEach(function (item) {
-  item.addEventListener('click', function () {
-    itemMenu.forEach(function (item) {
-      item.classList.remove('active');
+if (window.innerWidth > 750) {
+  let itemMenu = document.querySelectorAll('.menu ul li a');
+  itemMenu.forEach(function (item) {
+    item.addEventListener('click', function () {
+      itemMenu.forEach(function (item) {
+        item.classList.remove('active');
+      });
+      item.classList.add('active');
     });
-    item.classList.add('active');
   });
-});
+}
 // Final
 
 /*
  * The function aims to smooth out the scroll when the user clicks on the menu item and return to the top of the page when the "home" item has been clicked.
  */
 document.addEventListener('DOMContentLoaded', function () {
-  var menuLinks = document.querySelectorAll('.menu a');
+  let menuLinks = document.querySelectorAll('.menu a');
 
   menuLinks.forEach(function (link) {
     link.addEventListener('click', function (event) {
       event.preventDefault();
-      var targetId = this.getAttribute('href').substring(1);
-      var targetElement = document.getElementById(targetId);
+      let targetId = this.getAttribute('href').substring(1);
+      let targetElement = document.getElementById(targetId);
 
       if (targetElement) {
         if (targetId === 'header') {
@@ -60,4 +64,26 @@ document.addEventListener('DOMContentLoaded', function () {
  */
 document.addEventListener('DOMContentLoaded', function () {
   document.body.classList.add('loaded');
+});
+// Final
+
+/*
+ * The JavaScript function presented is responsible for toggling the visibility of a hamburger menu. When the hamburger icon is clicked, the function adds or removes the active class, which modifies the appearance of the icon, transforming it into an "x". Additionally, the function also shows or hides the navigation menu.
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerMenu = document.querySelector('.menu-hbr');
+  const nav = document.querySelector('.menu ul');
+
+  hamburgerMenu.addEventListener('click', function () {
+    nav.classList.toggle('show');
+    hamburgerMenu.classList.toggle('active');
+  });
+
+  const menuItems = document.querySelectorAll('.menu ul li a');
+  menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener('click', function () {
+      nav.classList.remove('show');
+      hamburgerMenu.classList.remove('active');
+    });
+  });
 });
